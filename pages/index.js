00ca722f6897e807266360993c5373eaf434c550
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-03-12 18:25:55 -0700

    test commit for vercel

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,48 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+// import Container from '../components/container'
+// import MoreStories from '../components/more-stories'
+// import HeroPost from '../components/hero-post'
+// import Intro from '../components/intro'
+// import Layout from '../components/layout'
+// import { getAllPostsForHome } from '../lib/api'
+// import Head from 'next/head'
+// import { CMS_NAME } from '../lib/constants'
 
-export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index() {
   return (
     <>
-      <Layout preview={preview}>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          <h1>hello world riku!</h1>
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
+    <h1>hello world</h1>
+    <p>riku hatano</p>
     </>
   )
 }
+// export default function Index({ preview, allPosts }) {
+//   const heroPost = allPosts[0]
+//   const morePosts = allPosts.slice(1)
+//   return (
+//     <>
+//       <Layout preview={preview}>
+//         <Head>
+//           <title>Next.js Blog Example with {CMS_NAME}</title>
+//         </Head>
+//         <Container>
+//           <Intro />
+//           <h1>hello world riku!</h1>
+//           {heroPost && (
+//             <HeroPost
+//               title={heroPost.title}
+//               coverImage={heroPost.coverImage}
+//               date={heroPost.date}
+//               author={heroPost.author}
+//               slug={heroPost.slug}
+//               excerpt={heroPost.excerpt}
+//             />
+//           )}
+//           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+//         </Container>
+//       </Layout>
+//     </>
+//   )
+// }
 
 export async function getStaticProps({ preview = false }) {
   const allPosts = (await getAllPostsForHome(preview)) ?? []
